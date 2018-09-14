cd199f1fa4eae9118a796125cba09b4040474c04
Douglas Detoni <ddetoni@thoughtworks.com>
2018-09-14 10:40:12 -0300

    First commit

---
+++ src/main/java/cl/sample/spotidentity/SpotidentityApplication.java
@@ -0,0 +1,12 @@
+package cl.sample.spotidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotidentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotidentityApplication.class, args);
+	}
+}
---
+++ src/test/java/cl/sample/spotidentity/SpotidentityApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.sample.spotidentity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotidentityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
